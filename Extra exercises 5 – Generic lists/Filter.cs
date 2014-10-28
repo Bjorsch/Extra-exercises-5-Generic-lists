d427a1ae6ea083fe4f86e2afff282243c4c85a4c
Bjorsch <johanbjork47@hotmail.com>
2014-10-28 14:23:19 +0100

    First Commit

---
+++ Extra exercises 5 – Generic lists/Filter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Extra_exercises_5___Generic_lists
+{
+    class Filter
+    {
+        /*public List<int> GetNumbersBeetween5and10(List<int> num1)
+        {
+            List<int> getNumber = new List<int>();
+            foreach (var number in num1)
+            {
+                if (number >= 5 && number <= 10)
+                {
+                    getNumber.Add(number);
+                }
+            }
+            return getNumber;
+        }
+
+        public int GetMaxValue(List<int> num1)
+        {
+
+
+            var maxValue = int.MinValue;
+
+            foreach (var number in num1)
+            {
+                if (number > maxValue)
+                {
+                    maxValue = number;
+                }
+            }
+            return maxValue;
+
+        }
+
+
+        public int Sum(List<int> num1)
+        {
+            var summa = 0;
+
+            foreach (var number in num1)
+            {
+                summa = sum + number;
+            }
+            return summa;
+        }
+         */   
+        public List <int> TakeFive(List<int> num1)
+        {
+            List<int> list = new List<int>();
+            
+            var listOfFive = 0;
+
+            foreach (var item in num1)
+            {
+                list.Add(item);
+                listOfFive++;
+
+                if (listOfFive == 5)
+                {
+                    break;
+                }
+                
+            }
+            return list;
+        }
+
+        public List<int> RemoveDuplicates (List<int> num1)
+        {
+            List<int> list = new List<int>();
+
+
+            foreach (var item in num1)
+            {
+                if(list.Contains(item) == false)
+                {
+                    list.Add(item);
+                }
+            }
+            return list;
+
+        }
+        public List<int> GetCommon (List<int> num1, List<int> num2)
+        {
+            List<int> list = new List<int>();
+
+            foreach (var item in num1)
+            {
+                if (num2.Contains(item))
+                {
+                    list.Add(item);
+                }
+
+            }
+            return list;
+            
+        }
+        public List<int> GetDifferences(List<int> num1, List<int> num2)
+        {
+            List<int> list = new List<int>();
+
+            foreach (var item in num1)
+            {
+                if (!num2.Contains(item))
+                {
+                    list.Add(item);
+                }
+
+            }
+            return list;
+        }
+    }
+
+}
+
+
---
+++ Extra exercises 5 – Generic lists/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Extra_exercises_5___Generic_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Filter filter = new Filter();
+
+            var nums1 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10  };
+            var nums2 = new List<int> { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            /*List<int> resultat = filter.GetNumbersBeetween5and10(nums1);
+
+            foreach (var number in resultat)
+            {
+                Console.WriteLine( number);
+            }
+            
+
+            Console.WriteLine("Högsta: " + filter.GetMaxValue(nums1));
+            Console.WriteLine();
+            Console.WriteLine("Summa: " + filter.Sum(nums1));
+
+            */
+
+            Console.WriteLine("Fem första: ");
+            var count = filter.TakeFive(nums1);
+            {
+                foreach (var item in count)
+                {
+                    Console.WriteLine(item);
+                }
+	
+            }
+
+
+            Console.WriteLine();
+            var RemoveDuplicates = filter.RemoveDuplicates(nums1);
+            {
+                foreach (var item in RemoveDuplicates)
+                {
+                    Console.WriteLine(item);
+                }
+	
+            }
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+            var getcommon = filter.GetCommon(nums1, nums2);
+
+            foreach (var item in getcommon)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+            var getDiff = filter.GetDifferences(nums1, nums2);
+
+            foreach (var item in getDiff)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+}
